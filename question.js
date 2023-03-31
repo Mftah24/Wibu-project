e8dbfcbe8640e9b36cb4b1aa3dc4aab5a6e1e19c
Miftahul Amri <129349560+Mftah24@users.noreply.github.com>
2023-03-31 19:31:58 +0700

    Add files via upload

---
+++ question.js
@@ -0,0 +1,5 @@
+let nama = prompt("siapa waifu tercantik menurut kamu?")
+
+alert("Walaupun menurut mu "+ nama + " adalah waifu tercantik, tapi sayang sekali belum bisa mengalahkan kecantikan dan keimutan kuriyama miraii;") 
+
+alert("salam hangat, miftah (Author In this Isekai)")
